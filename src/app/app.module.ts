9a4b397c8289c5dc6a1f4f2ec27b2f73b36c8c5f
Ian Findlay <code@findlayian.com>
2022-07-15 19:11:27 +0100

    Change button to material button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { JourneyComponent } from './journey/journey.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatButtonModule} from '@angular/material/button'
 import {MatInputModule} from '@angular/material/input'
 
 
@@ -18,6 +19,7 @@ import {MatInputModule} from '@angular/material/input'
     BrowserModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     MatInputModule,
   ],
   providers: [],
