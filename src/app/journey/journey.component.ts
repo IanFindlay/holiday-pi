3ea26452e56365af821dfcc8ca3188fc6f195bba
Ian Findlay <code@findlayian.com>
2022-07-17 12:24:11 +0100

    Fix journey message formatting

--- src/app/journey/journey.component.ts
+++ src/app/journey/journey.component.ts
@@ -68,19 +68,18 @@ export class JourneyComponent implements OnInit {
     const suffix = numPassenger > 4 ? 's' : '';
 
     if (taxiCost === carCost)
-      message = `Taxi${suffix} or car${suffix}... I estimate that it won't matter as both will cost about £${taxiCost.toFixed(
-        2
-      )}`;
+      message = `Taxi${suffix} or car${suffix}... I estimate that it won't matter
+                 as both will cost about £${taxiCost.toFixed(2)}`;
     else if (taxiCost < carCost)
-      message = `Taxi${suffix} will be cheaper costing about £${taxiCost.toFixed(
-        2
-      )} which is £${(carCost - taxiCost).toFixed(2)}
-               less than taking the car${suffix}`;
+      message = `Taxi${suffix} will be cheaper costing about
+                 £${taxiCost.toFixed(2)} which is
+                 £${(carCost - taxiCost).toFixed(2)} less than
+                 taking the car${suffix}`;
     else
-      message = `Car${suffix} will be cheaper costing about £${carCost.toFixed(
-        2
-      )} which is £${(taxiCost - carCost).toFixed(2)}
-               less than taking the taxi${suffix}`;
+      message = `Car${suffix} will be cheaper costing about
+                 £${carCost.toFixed(2)} which is
+                 £${(taxiCost - carCost).toFixed(2)} less than
+                 taking the taxi${suffix}`;
 
     return message;
   }
